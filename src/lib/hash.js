737272bf57cfe135505f88ac0400ea822e9f76a2
Benjamin Baldivia <goblinlordx@gmail.com>
2020-02-01 19:28:19 +0900

    Implement basic hashing algorithm

---
+++ src/lib/hash.js
@@ -0,0 +1,24 @@
+// Slightly modified java based hash (XOR with max after each iteration)
+const hash = (value = 0) => {
+  const str =
+    typeof value === "number"
+      ? "number:" + value.toString(36)
+      : typeof value === "string"
+      ? "string:" + value
+      : null;
+
+  if (str === null) {
+    console.log(str, typeof str);
+    throw new Error("Invalid input value (should be string, number)");
+  }
+
+  return str.split("").reduce((a, chr) => {
+    a = (a << 5) - a + chr.charCodeAt(0);
+    a = (a & a) + (a >>> 31);
+    a = a & 0x7fffffff;
+    a = a ^ 0x7fffffff;
+    return a;
+  }, 0);
+};
+
+export default hash;
---
+++ src/lib/hash.test.js
@@ -0,0 +1,44 @@
+import hash from "./hash";
+
+it("runs with no parameters", () => {
+  expect(hash()).toBeTruthy();
+});
+
+it("runs generates hash value different from input", () => {
+  const input = 0;
+  expect(hash(input)).not.toBe(input);
+});
+
+it("runs generates numeric hash value", () => {
+  const input = 0;
+  expect(typeof hash(input)).toBe("number");
+});
+
+it("runs can accept a string", () => {
+  const input = "test value";
+  expect(typeof hash(input)).toBe("number");
+});
+
+const diffPairs = [
+  [0, 1],
+  [-1, 1],
+  [1, 2],
+  [1000, 1001],
+  ["2", 2],
+  ["Test", "Test1"],
+  ["abcd", "abce"],
+  ["abcd", "bbcd"]
+];
+const printPair = (left, right) =>
+  `${typeof left}(${left}), ${typeof right}(${right})`;
+
+diffPairs.forEach(([left, right]) => {
+  it(`generates a different hash for the values: ${printPair(
+    left,
+    right
+  )}`, () => {
+    const leftResult = hash(left);
+    const rightResult = hash(right);
+    expect(leftResult).not.toBe(rightResult);
+  });
+});
