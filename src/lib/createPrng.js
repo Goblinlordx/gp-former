a887a41c9a403d710df2ebfc9f349b6e917d7a29
Benjamin Baldivia <goblinlordx@gmail.com>
2020-02-01 21:06:46 +0900

    Implement PRNG

---
+++ src/lib/createPrng.js
@@ -0,0 +1,22 @@
+// Park-Miller PCG (ref: http://www.firstpr.com.au/dsp/rand31/)
+const A = 16807; // Multiplier
+const MAX = 0x7fffffff; // (2^31) - 1
+
+const createPrng = (seed = 0) => {
+  let current = seed % MAX;
+  if (current <= 0) current += MAX - 1;
+
+  const generate = () => {
+    current = (current * A) % MAX;
+    return (current - 1) / MAX; // Always output float
+  };
+
+  let i = 0;
+  let itr = 0;
+  // Prerun (5x then (0-100)x)
+  for (i = 0, itr = 5; i < itr; i++) generate();
+  for (i = 0, itr = current % 100; i < itr; i++) generate();
+  return generate;
+};
+
+export default createPrng;
---
+++ src/lib/createPrng.test.js
@@ -0,0 +1,51 @@
+import createPrng from "./createPrng";
+
+it("runs with no parameters", () => {
+  const rng = createPrng();
+  expect(rng).toBeTruthy();
+});
+
+const printArr = arr => arr.join(", ");
+
+const comparisonSeeds = [
+  [0, 1],
+  [1, 2],
+  [1, 10]
+];
+
+const getIterations = (rng, cycles) =>
+  Array(cycles)
+    .fill(0)
+    .map(rng);
+
+comparisonSeeds.forEach(([left, right]) => {
+
+  it(`generates different numbers from different seeds: ${printArr([
+    left,
+    right
+  ])}`, () => {
+    const lPrng = createPrng(left);
+    const rPrng = createPrng(right);
+
+    const leftSet = new Set(getIterations(lPrng, 1000));
+    const rightSet = new Set(getIterations(rPrng, 1000));
+    const match = [...rightSet].some(v => leftSet.has(v));
+    expect(match).not.toBeTruthy();
+  });
+});
+
+const seedRepeats = [0, 1, 100, 3211, 0xFF, 0xFFFF];
+
+seedRepeats.forEach(testSeed => {
+  it(`generates a non-repeating set of values: ${testSeed}`, () => {
+    const prng = createPrng(testSeed);
+
+    const sequence1 = new Set(getIterations(prng, 5000));
+    const sequence2 = new Set(getIterations(prng, 5000));
+
+    const match = [...sequence1].some(v => sequence2.has(v));
+    expect(match).not.toBeTruthy();
+  })
+});
+
+// TODO [BMB]: Test for even distribution in generated values
