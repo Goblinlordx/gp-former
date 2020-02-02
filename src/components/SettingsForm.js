310e9e2e227162c7bbda103a212ab1465c56b9d0
Benjamin Baldivia <goblinlordx@gmail.com>
2020-02-02 16:33:14 +0900

    Add randomize button, adjust style

--- src/components/SettingsForm.js
+++ src/components/SettingsForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { Section } from "./Common";
 
 const handleSubmit = (submit = () => {}) => e => {
@@ -7,17 +8,32 @@ const handleSubmit = (submit = () => {}) => e => {
   return submit(Object.fromEntries(data.entries()));
 };
 
+const FieldLabel = styled.span`
+  margin-right: 1em;
+  font-weight: bold;
+`;
+
 export default ({ data, loading, onInput, onSubmit }) => {
   const handleChange = name => e => {
     e.preventDefault();
     const v = e.target.value;
     return onInput({ ...data, [name]: v });
   };
+
+  const handleRandomize = e => {
+    const randomSeed = Math.floor(Math.random() * 36 ** 5).toString(36);
+    handleChange("seed")({
+      target: { value: randomSeed },
+      preventDefault: e.preventDefault.bind(e)
+    });
+    onSubmit(e);
+  };
+
   return (
     <Section>
       <form style={{ margin: "0.5em" }} onSubmit={handleSubmit(onSubmit)}>
         <label>
-          Seed
+          <FieldLabel>Seed</FieldLabel>
           <input
             type="text"
             name="seed"
@@ -27,6 +43,7 @@ export default ({ data, loading, onInput, onSubmit }) => {
           />
         </label>
         <input type="submit" label="Generate" />
+        <button onClick={handleRandomize}>Randomize</button>
       </form>
     </Section>
   );
