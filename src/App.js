7e2e16f913df0fd02d0752f416c18bc4b1a5ce16
Benjamin Baldivia <goblinlordx@gmail.com>
2020-02-02 20:44:25 +0900

    Remove unused

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import About from "./components/About";
 import SettingsForm from "./components/SettingsForm";
--- src/components/LoadableSetupDisplay.js
+++ src/components/LoadableSetupDisplay.js
@@ -1,6 +1,5 @@
 import React from "react";
 import GaiaProjectSetupDisplay from "./GaiaProjectSetupDisplay";
-import useGPRandomizer from "../hooks/useGPRandomizer";
 
 export default ({setup, loading, error}) => {
   return loading ? (
