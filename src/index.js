1dce40be30a8dc5767fa3e61ef33bbe14ecee7d2
Benjamin Baldivia <goblinlordx@gmail.com>
2020-02-03 22:34:00 +0900

    Hydrate prerendered app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
