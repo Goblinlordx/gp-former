8218ae852584b169cc9f0ae9b5d82adabef1cf09
Benjamin Michael Baldivia <goblinlordx@gmail.com>
2021-03-21 21:35:23 +0900

    convert createQueryString to ts

--- src/libs/createQueryString.ts
+++ src/libs/createQueryString.ts
@@ -1,4 +1,4 @@
-export default (obj) =>
+export default (obj: Record<string, string | number | boolean>) =>
   Object.entries(obj)
     .reduce((a, [k, v]) => {
       a.append(encodeURIComponent(k), encodeURIComponent(v));
